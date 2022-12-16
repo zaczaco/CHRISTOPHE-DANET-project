193778b01d2832e552fa82e33d04336f32a6777a
ZAC <alexandre@hdvma.com>
2022-12-16 13:30:12 +0100

    cm

--- realpj/controllers/home.route.js
+++ realpj/controllers/home.route.js
@@ -5,15 +5,4 @@ const router = express.Router();
 // http://localhost:9000/hello
 router.get('/', (req, res) => res.render('home_view'));
 
-// http://localhost:9000/hello/world
-router.get('/world', (req, res) => {
-    //res.send('Hello WORLD, from controller...');
-    res.render('hello_view', { favourites: [
-        { category: 'cheese', thing: 'raclette' },
-        { category: 'book', thing: 'Umberto Eco: Foucaults pendulum' },
-        { category: 'drink', thing: 'cidre brut' },
-        { category: 'color', thing: 'green (allez les Verts! ;) ' },
-    ] });
-});
-
 module.exports = router;
